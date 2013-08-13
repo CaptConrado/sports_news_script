eaa3db63e45aeba2b8d83868dd3514b6f3b45ea7
CaptConrado <ottey001@gmail.com>
2013-08-12 19:36:02 -0700

    cleanup

--- game_test.rb
+++ game_test.rb
@@ -2,7 +2,14 @@ require 'test/unit'
 require 'game.rb'
 
 class GameTest < Test::Unit::TestCase
+	def test_if_winner_works
+		home_score = "9"
+		away_score = "42"
+		game = Game.new("Chargers","Dodgers",home_score,away_score)
 
-	
+		game.winner.should == "Dodgers"
+		# assert_equal == "Dodgers"
+	end
+
 
 end
