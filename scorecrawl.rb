4a808f43ef777f212b410135890c6943e12c518f
CaptConrado <ottey001@gmail.com>
2013-08-13 15:56:31 -0700

    added some parsing

--- scorecrawl.rb
+++ scorecrawl.rb
@@ -21,10 +21,14 @@ puts PAGE_URL
 
 page = doc.get(PAGE_URL)
 counter = 0
-page.body.split("\n").each do |k|
+page.body.split("<table class=\"sbgame\">").each do |k|
 	line = k.chomp
-
+	puts "~~~"
+	puts line.split("\n").count 
+	puts "~~~~~"
 	if line.match(/<td width="20%" align="right">(.+?)<\/td>/)
 		puts $1
 	end
 end
+
+
